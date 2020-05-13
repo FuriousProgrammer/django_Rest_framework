6a3dc090e5f2be7d07f8e49ec57e6c4f394c3b82
Sadia <sadiagondal123@gmail.com>
2020-05-13 21:31:06 +0500

    Rest Framwork api_view() decorator Added

--- r_api/views.py
+++ r_api/views.py
@@ -4,42 +4,43 @@ from django.http import HttpResponse, JsonResponse
 from .models import Artical
 from.serializer import ArticalSerializer
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def artical_list(request):
     if request.method == 'GET':
         artical = Artical.objects.all()
         serializer = ArticalSerializer(artical, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ArticalSerializer(data=data)
+        serializer = ArticalSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT','DELETE'])
 def artical_detail(request, pk):
 
     try:
         artical = Artical.objects.get(pk =  pk)
     except artical.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
     if request.method == 'GET':
         serializer = ArticalSerializer(artical)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ArticalSerializer(artical, data=data)
+        serializer = ArticalSerializer(artical, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=404)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
     elif request.method == 'DELETE':
         artical.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
